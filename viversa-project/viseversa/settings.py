c0c00d7f7eeae553611f14bdcac6af819bb3c0f4
vitosv03 <vitosv03@gmail.com>
2021-11-18 22:17:13 +0200

    add to settings.py DIRS ['templates']

--- viversa-project/viseversa/settings.py
+++ viversa-project/viseversa/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'viseversa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
