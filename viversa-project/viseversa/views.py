9d541820f8e7b2a94667cd9cc696a536db75df00
vitosv03 <vitosv03@gmail.com>
2021-11-18 22:49:04 +0200

    add directory templates add home.html into templates

--- viversa-project/viseversa/views.py
+++ viversa-project/viseversa/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def about(request):
     return HttpResponse('This is about page')
 
-def home(request):
-    return HttpResponse('<h1>This is home page</h1>')
 
+def home(request):
+    return render(request, 'home.html', context= {'greeting': 'Hello!'})
